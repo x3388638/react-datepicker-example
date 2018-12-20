db8dc476b25907c645e72cec4d309e2427852846
YY <z3388638@gmail.com>
2018-12-20 23:54:30 +0800

    add Calendar, Switch, Table component

---
+++ src/Calendar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import moment from 'moment'
+import styled from 'styled-components';
+
+import Switch from './Switch';
+import Table from './Table';
+
+const Container = styled.div`
+	display: inline-block;
+	border: 1px solid #afafaf;
+	padding: 20px 15px 40px 15px;
+	box-shadow: 0 0 10px 0px #afafaf;
+`;
+
+export default class Calendar extends React.PureComponent {
+	constructor (props) {
+		super(props);
+		const [year, month, day] = moment().format('YYYY-MM-DD').split('-');
+		this.state = {
+			year,
+			month,
+			day
+		};
+	}
+
+	render() {
+		return (
+			<Container>
+				<Switch />
+				<Table />
+			</Container>
+		);
+	}
+}
---
+++ src/Switch.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Switch extends React.PureComponent {
+	render() {
+		return <div>switch</div>;
+	}
+}
