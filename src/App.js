ec8e1604c1668432284d76b877ccb8a463914f30
YY <z3388638@gmail.com>
2018-12-21 00:35:03 +0800

    add fontawesome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Calendar from './Calendar';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+library.add([faChevronLeft, faChevronRight])
+
 export default () => (
 	<div style={{ padding: '30px' }}>
 		<Calendar />
--- src/Switch.js
+++ src/Switch.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Container = styled.div`
+	width: 200px;
+	display: flex;
+`;
+
+const Prev = styled.span`
+	flex: 1;
+	text-align: center;
+`;
+
+const Text = styled.span`
+	flex: 5;
+	text-align: center;
+`;
+
+const Next = styled.span`
+	flex: 1;
+	text-align: center;
+`;
 
 export default class Switch extends React.PureComponent {
 	render() {
-		return <div>switch</div>;
+		return (
+			<Container>
+				<Prev><FontAwesomeIcon icon="chevron-left" /></Prev>
+				<Text>May 2018</Text>
+				<Next><FontAwesomeIcon icon="chevron-right" /></Next>
+			</Container>
+		)
 	}
 }
