8ccc0d9cef67554a80ecf2d73004ccde35416ae2
YY <z3388638@gmail.com>
2018-12-22 01:33:40 +0800

    add YearTable & prototype

--- src/TableContainer.js
+++ src/TableContainer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import DayTable from './DayTable';
 import MonthTable from './MonthTable';
+import YearTable from './YearTable';
 
 export default class TableContainer extends React.PureComponent {
 	render() {
-		return <MonthTable />
+		return <YearTable />
 	}
 }
---
+++ src/YearTable.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Table = styled.table`
+	width: 200px;
+	table-layout: fixed;
+`;
+
+const Grid = styled.td`
+	text-align: center;
+	padding-top: 20px;
+	cursor: pointer;
+	color: ${(props) => props.outside ? '#eee' : '#000'}
+`;
+
+export default class YearTable extends React.PureComponent {
+	render() {
+		return (
+			<Table>
+				<tbody>
+					<tr>
+						<Grid outside>2009</Grid>
+						<Grid>2010</Grid>
+						<Grid>2011</Grid>
+						<Grid>2012</Grid>
+					</tr>
+					<tr>
+						<Grid>2013</Grid>
+						<Grid>2014</Grid>
+						<Grid>2015</Grid>
+						<Grid>2016</Grid>
+					</tr>
+					<tr>
+						<Grid>2017</Grid>
+						<Grid>2018</Grid>
+						<Grid>2019</Grid>
+						<Grid outside>2020</Grid>
+					</tr>
+				</tbody>
+			</Table>
+		)
+	}
+}
